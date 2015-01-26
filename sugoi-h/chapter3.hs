9bef14ec80e201821fb6449ce88eee6967ebac03
Shuichiro Kamiya <notsunohito@gmail.com>
2015-01-26 22:44:14 +0900

    where \n

--- sugoi-h/chapter3.hs
+++ sugoi-h/chapter3.hs
@@ -95,7 +95,8 @@ bmiTell weight height
         | bmi <= normal  = "You're supposedly normal, Pffft, I bet you're ugly!"
         | bmi <= fat           = "You're fat! Lose some weight, fatty!"
         | otherwise           = "You're a whale, congratulations!"
-        where bmi        = weight / height ^ 2
-                     skinny   = 18.5
-                     normal = 25.0
-                     fat          = 30.0
+        where
+                bmi        = weight / height ^ 2
+                skinny   = 18.5
+                normal = 25.0
+                fat          = 30.0
