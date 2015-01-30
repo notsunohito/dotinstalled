2bbd8aac9572f7c435c794a7fda1260fe910f283
Shuichiro Kamiya <notsunohito@gmail.com>
2015-01-30 22:57:08 +0900

    Add chapter4.hs

---
+++ sugoi-h/chapter4.hs
@@ -0,0 +1,13 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list!"
+maximum' [x] = x
+maximum' (x : xs) = max x (maximum' xs)
+
+-- My Answer
+replicate'' :: Int -> a -> [a]
+replicate'' 1 x = (x : [])
+replicate'' n x  = x :  replicate (n - 1) x
+-- Answer
+replicate' n x
+               |n <= 0 = []
+               | otherwise = x : replicate' (n - 1) x
