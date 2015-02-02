2f164866429f984dfdf816c1e7095cbc6d1093ed
Notsunohito <notsunohito@gmail.com>
2015-02-02 13:01:37 +0900

    in js

--- sugoi-h/my_recursion.hs
+++ sugoi-h/my_recursion.hs
@@ -3,3 +3,8 @@ doubleNums [] = []
 doubleNums (x : xs) = [x *2] ++ doubleNums xs
 -- doubleNums [1,2,3,4,5]
 -- => [2,4,6,8,10]
+
+-- function doubleNums (list) {
+--     if (list.length === 0) return [];
+--     return [ list[0] * 2 ].concat( doubleNums( list.slice(1) ) );
+-- }
