9b312c2054c05866cff042c16a2a6bb63ec365cb
Shuichiro Kamiya <notsunohito@gmail.com>
2015-02-07 13:38:50 +0900

    Learning scanl/scanr, ($)

--- sugoi-h/chapter5.hs
+++ sugoi-h/chapter5.hs
@@ -250,3 +250,30 @@ last' = foldl1 (\_ x -> x)
 -- foldl g z [3,4,5,6]
 -- => g (g (g (g (g z 3) 4) 5) 6
 
+and' :: [Bool] -> Bool
+and' xs = foldr (&&) True xs
+
+-- and' [True,True,True]
+-- => True
+-- scanl (+) 0 [3,5,2,1]
+-- => [0,3,8,10,11]
+-- scanr (+) 0 [3,5,2,1]
+-- => [11,8,3,1,0]
+-- scanl1 (\acc x -> if x > acc then x else acc) [3,4,5,3,7,9,2,1]
+-- => [3,4,5,5,7,9,9,9]
+-- scanl (flip (:)) [] [3,2,1]
+-- => [[],[3],[2,3],[1,2,3]]
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+
+-- sqrtSums
+-- => 131
+-- sum (map sqrt [1..131])
+-- => 1005.0942035344083
+-- sum (map sqrt [1..130])
+-- => 993.6486803921487
+
+-- :t ($)
+-- => ($) :: (a -> b) -> a -> b
+-- => f $ x = f x
