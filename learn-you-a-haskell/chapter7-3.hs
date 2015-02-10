e7da1ff41b930f1111e490bdf01b9b2b4b90d68d
Notsunohito <notsunohito@gmail.com>
2015-02-10 18:00:50 +0900

    YesNo

--- learn-you-a-haskell/chapter7-3.hs
+++ learn-you-a-haskell/chapter7-3.hs
@@ -26,7 +26,76 @@ instance Show TrafficLight where
 -- [Red, Yellow, Green]
 -- => [Red light,Yellow light,Green light]
 
-instance (Eq m) => Eq (Maybe m) where
-    Just x == Just y = x == y
-    Nothing == Nothing = True
-    _ == _ = False
+-- instance (Eq m) => Eq (Maybe m) where
+--     Just x == Just y = x == y
+--     Nothing == Nothing = True
+--     _ == _ = False
+
+-- :info Maybe
+-- => data Maybe a = Nothing | Just a 	-- Defined in eData.Maybef
+--    instance Eq a => Eq (Maybe a) -- Defined in eData.Maybef
+--    instance Monad Maybe -- Defined in eData.Maybef
+--    instance Functor Maybe -- Defined in eData.Maybef
+--    instance Ord a => Ord (Maybe a) -- Defined in eData.Maybef
+--    instance Read a => Read (Maybe a) -- Defined in eGHC.Readf
+--    instance Show a => Show (Maybe a) -- Defined in eGHC.Showf
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _  = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing  = False
+
+-- instance YesNo (Tree a) where
+--     yesno EmptyTree = False
+--     yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
+
+-- yesno $ length []
+-- => False
+-- yesno "haha"
+-- => True
+-- yesno ""
+-- => False
+-- yesno $ Just 0
+-- => True
+-- yesno True
+-- => True
+-- yesno []
+-- => False
+-- yesno [0,0,0]
+-- => True
+-- :t yesno
+-- => yesno :: YesNo a => a -> Bool
+
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult =
+    if yesno yesnoVal
+    then yesResult
+    else noResult
+
+-- yesnoIf [] "YEAH!" "NO!"
+-- => "NO!"
+-- yesnoIf [2,3,4] "YEAH!" "NO!"
+-- => "YEAH!"
+-- yesnoIf True "YEAH!" "NO!"
+-- => "YEAH!"
+-- yesnoIf (Just 500) "YEAH!" "NO!"
+-- => "YEAH!"
+-- yesnoIf Nothing "YEAH!" "NO!"
+-- => "NO!"
